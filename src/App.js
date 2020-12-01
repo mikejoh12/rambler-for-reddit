6483388b834d8a62735f80fe921d4c2043624838
Michael Johansson <mikejoh12@gmail.com>
2020-11-30 21:05:45 -0600

    Add Material UI layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { CategoriesList } from './features/CategoriesList/CategoriesList';
-import { Counter } from './features/reddit/Counter';
+import React from 'react'
+import { CategoriesList } from './features/CategoriesList/CategoriesList'
+import { Counter } from './features/reddit/Counter'
 import { Header } from './features/Header/Header'
 import { PostList } from './features/PostList/PostList'
+import { Grid } from '@material-ui/core'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <PostList />
-        <CategoriesList />
-        <Counter />
-      </header>
-    </div>
-  );
-}
+      <div className="App">
+        <Grid container style={{background: 'lightblue'}}>
+            <Grid item xs={12} style={{background: 'gray'}}>
+              <Header />
+            </Grid>
+            <Grid item xs={6}>
+              <PostList />
+            </Grid>
+            <Grid item xs={6}>
+              <CategoriesList />
+            </Grid>
+            <Grid item xs={12}>
+              <Counter />
+            </Grid>
+        </Grid>
+      </div>
+    );
+  }
+
 
 export default App;
