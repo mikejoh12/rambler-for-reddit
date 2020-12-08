a07ea24880234fa0c49d39cd4f43fecda593ff46
Michael Johansson <mikejoh12@gmail.com>
2020-12-07 21:18:13 -0600

    Add more test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import App from './App'
-const { shallow } = require("enzyme")
+import store from './app/store';
+import { Provider } from 'react-redux';
+
+const { shallow, mount } = require("enzyme")
+
 
 describe('App component', () => {
   it('renders without crashing', () => {
     shallow(<App />)
+  })
+})
+
+describe('App Full DOM Rendering', () => {
+  it('renders without crashing', () => {
+    mount(<Provider store={store}>
+            <App />
+          </Provider>)
   })
 })
